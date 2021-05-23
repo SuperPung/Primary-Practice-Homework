3634d72ff40e6f067cf673ae0ef4572d623bc5fa
SUPER <superpung.lee@gmail.com>
2021-05-23 12:56:24 +0800

    add Simulate.java

---
+++ src/com/huawei/classroom/student/h61/Simulate.java
@@ -0,0 +1,12 @@
+package com.huawei.classroom.student.h61;
+
+/**
+ * @author super
+ */
+public class Simulate {
+
+    public SimResult sim(Param param, int days) {
+        SimResult result = new SimResult();
+        return null;
+    }
+}
