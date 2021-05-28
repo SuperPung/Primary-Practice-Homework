f5427847e8196dd691c831beae829f14dfe106c1
SUPER <superpung.lee@gmail.com>
2021-05-28 17:25:35 +0800

    feat(mydaemon): update

--- src/com/huawei/classroom/student/h62/MyDaemon.java
+++ src/com/huawei/classroom/student/h62/MyDaemon.java
@@ -1,11 +1,19 @@
 package com.huawei.classroom.student.h62;
 
+import java.util.List;
+
 /**
  * @author super
  */
 public class MyDaemon extends Thread {
+    private String root;
+    private int port;
+    private List<String> users;
+
     public MyDaemon(MyDeamonConfigVo config) {
-        super();
+        root = config.getRoot();
+        port = config.getPort();
+        users = config.getUsers();
     }
 
     @Override
--- src/com/huawei/classroom/student/h62/MyDeamonConfigVo.java
+++ src/com/huawei/classroom/student/h62/MyDeamonConfigVo.java
@@ -1,12 +1,16 @@
 package com.huawei.classroom.student.h62;
 
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @author super
  */
 public class MyDeamonConfigVo {
     private String root;
     private int port;
-    private String passwordFile;
+    private List<String> users = new ArrayList<>();
 
     public MyDeamonConfigVo() {
         super();
@@ -14,6 +18,10 @@ public class MyDeamonConfigVo {
 
     public void setRoot(String root) {
         this.root = root;
+    }
+
+    public String getRoot() {
+        return root;
     }
 
     public void setPort(int port) {
@@ -24,7 +32,30 @@ public class MyDeamonConfigVo {
         return port;
     }
 
-    public void setPasswordFile(String passwordFile) {
-        this.passwordFile = passwordFile;
+    public void setPasswordFile(String passwordFile) throws IOException {
+        this.users = readLines(passwordFile);
+    }
+
+    public List<String> getUsers() {
+        return users;
+    }
+
+    private List<String> readLines(String filePath) throws IOException {
+        List<String> result = new ArrayList<>();
+        String line;
+        Reader reader = new FileReader(filePath);
+        LineNumberReader lineReader = new LineNumberReader(reader);
+
+        while (true) {
+            line = lineReader.readLine();
+            if (line == null) {
+                break;
+            }
+            if (line.trim().length() == 0 || line.startsWith("#")) {
+                continue;
+            }
+            result.add(line);
+        }
+        return result;
     }
 }
